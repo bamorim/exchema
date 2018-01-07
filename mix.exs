e6ff2b05812317564855483d13517e1b5726c4c0
Bernardo Amorim <contato@bamorim.com>
2018-01-07 21:40:02 -0200

    Added Hex Package Definition

--- mix.exs
+++ mix.exs
@@ -8,7 +8,8 @@ defmodule Exchema.Mixfile do
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
-      elixirc_paths: elixirc_paths(Mix.env)
+      elixirc_paths: elixirc_paths(Mix.env),
+      package: package()
     ]
   end
 
@@ -28,4 +29,14 @@ defmodule Exchema.Mixfile do
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_),     do: ["lib"]
+
+  defp package do
+    [
+      name: "exchema",
+      description: "Exchema is a library to build schemas that can coerce and validate input",
+      licenses: ["Apache 2.0"],
+      maintainers: ["Bernardo Amorim"],
+      links: %{"GitHub" => "https://github.com/bamorim/exchema"}
+    ]
+  end
 end
