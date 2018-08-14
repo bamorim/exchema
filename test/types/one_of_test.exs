0dfa6c1235e9b9b4bc1ccb9ef995635f16d28c64
Bernardo Amorim <contato@bamorim.com>
2018-08-14 16:51:01 -0300

    fix: update test for new one of error

--- test/types/one_of_test.exs
+++ test/types/one_of_test.exs
@@ -1,6 +1,6 @@
 defmodule Exchema.Types.OneOfTest do
   use ExUnit.Case
-  
+
   alias Exchema.Types, as: T
 
   @extype {T.OneOf, [T.Integer, T.String, T.Date]}
@@ -8,12 +8,20 @@ defmodule Exchema.Types.OneOfTest do
   test "it allows all the specified types" do
     assert Exchema.is?(1, @extype)
     assert Exchema.is?("s", @extype)
-    assert Exchema.is?(Date.utc_today, @extype)
+    assert Exchema.is?(Date.utc_today(), @extype)
     refute Exchema.is?(1.0, @extype)
-    refute Exchema.is?(DateTime.utc_now, @extype)
+    refute Exchema.is?(DateTime.utc_now(), @extype)
   end
 
   test "it returns a simple error" do
-    assert [{_, _, :invalid_type}] = Exchema.errors(1.0, @extype)
+    assert [
+             {_, _,
+              {:nested_errors,
+               [
+                 {Exchema.Types.Integer, [{_, _, :not_an_integer}]},
+                 {Exchema.Types.String, [{_, _, :not_a_binary}]},
+                 {Exchema.Types.Date, [{_, _, :not_a_struct}]}
+               ]}}
+           ] = Exchema.errors(1.0, @extype)
   end
 end
